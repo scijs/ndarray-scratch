5f7c06444e11011bfac4840b7df075df0da7cd73
Mikola Lysenko <mikolalysenko@gmail.com>
2013-07-01 21:45:32 -0500

    0.0.1

--- test/test.js
+++ test/test.js
@@ -7,6 +7,8 @@ require("tape")("ndarray-scratch", function(t) {
   var x = pool.malloc([10,10])
   
   t.same(x.shape, [10,10])
+  t.same(x.stride, [10,1])
+  t.assert(x.data.length >= 10*10)
   
   pool.free(x)
 
